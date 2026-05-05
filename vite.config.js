9da0ac166aba8bb7a60542055904e714536ff3cc
pa360800-cmyk <pa360800@example.com>
2026-05-05 21:18:35 +0800

    ziggy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import {
-    defineConfig
-} from 'vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     server: {
         host: '127.0.0.1',
+    },
+    resolve: {
+        alias: {
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            '@': resolve(__dirname, 'resources/js'),
+        },
     },
     plugins: [
         laravel({
@@ -18,7 +23,4 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
-    esbuild: {
-        jsx: 'automatic',
-    },
 });
